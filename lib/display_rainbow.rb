880a153433ed849efda884928141f2955014799a
Phan Huynh Tan <tanb1509892@student.ctu.edu.vn>
2019-01-05 16:05:39 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(a)
-  puts "R: #{a[0]}, O: #{a[0]}, Y: #{a[0]}, G: #{a[0]}, B: #{a[0]}, I: #{a[0]}, V: #{a[0]}"
+  puts "R: #{a[0]}, O: #{a[1]}, Y: #{a[2]}, G: #{a[3]}, B: #{a[4]}, I: #{a[5]}, V: #{a[6]}"
 end
