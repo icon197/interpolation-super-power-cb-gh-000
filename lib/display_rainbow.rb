456f368b8567c69324349125fd30929c9bfddac7
Phan Huynh Tan <tanb1509892@student.ctu.edu.vn>
2019-01-05 16:05:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(a)
-  puts "#{a.uppercase}"
+  puts "R: #{a[0]}, O: #{a[0]}, Y: #{a[0]}, G: #{a[0]}, B: #{a[0]}, I: #{a[0]}, V: #{a[0]}"
+end
